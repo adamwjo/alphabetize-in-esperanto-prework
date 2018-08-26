57b81b115406d6850629a91b362bd49103fb3df4
Adam Johnson <adamwjo@gmail.com>
2018-08-26 17:00:41 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,5 @@
-def alphabetize(arr)
-  # code here
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+ascii = "@-\\"
+def alphabetize(array)
+  array.sort_by{|string| string.tr(ESPERANTO_ALPHABET, ascii)}
 end
